c16e1bf87dea3a0807c85d7f2d38212fd09427fc
smallyounth <3510056926@qq.com>
2025-09-10 12:12:06 +0800

    学习了两个交换机类型，FanoutExchange和DirectExchange两种，区别就是Direct的只会发送给Routingkey一致的queue中，其功能更加灵活。 并学习了两种队列和交换机声明及绑定的方法，一种是配置类，另一种是@RabbitListener注解。

--- consumer/src/main/java/cn/itcast/mq/listener/RabbitMessageListen.java
+++ consumer/src/main/java/cn/itcast/mq/listener/RabbitMessageListen.java
@@ -29,7 +29,7 @@ public class RabbitMessageListen {
     //直接通过@RabbitListener注解来实现绑定关系，注册队列及交换机，相比配置类更加便捷
     // 可以指定队列、交换机、路由键
     //@Queue @Exchange注解 可以声明队列和交换机
-    //@QueueBinding 注解 可以声明绑定关系并指定Routingkey
+    //@QueueBinding 注解 可以声明绑定关系并指定Routing key
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue("direct.queue1"),
             exchange = @Exchange(name = "itcast.direct", type = ExchangeTypes.DIRECT),
